6b33686ba1c33cae5f64f7f3574312f970b0c28d
SebastianRamos01 <sebasr.sr@hotmail.com>
2023-07-04 23:51:34 -0300

    Footer form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,35 +2,42 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <footer className='h-screen bg-blue flex justify-center flex-col'>
-        <div className='text-white ml-4'>
-            Send me a message
+    <footer className='h-screen bg-blue flex justify-center items-center flex-col'>
+        <div className='flex flex-col items-center'>
+            <div className='text-white text-2xl opacity-50'>
+                Your interested?
+            </div>
+            <div className='text-white text-4xl'>
+                Send me a message
+            </div>
         </div>
-        <form action="" className='w-80 border border-grey h-fit rounded flex items-center flex-col ml-4'>
+        <form action="" className='w-80 h-fit rounded flex items-center flex-col'>
             <div className='w-3/4'>
-                <div className='text-white'>
+                <div className='text-white z-50'>
                     Name
                 </div>
-                <input type="text" name="" id="" className='outline-none p-1 rounded w-full'/>
-            </div>
-            <div className='w-3/4'>
-                <div className='text-white'>
-                    Email
-                </div>
-                <div className='border-white'>
-                    <input type="text" name="" id="" className='outline-none p-1 rounded w-full'/>
+                <div className='border border-white'>
+                    <input type="text" name="" id="" className='outline-none p-1 bg-blue w-full text-white'/>
                 </div>
             </div>
             <div className='w-3/4'>
                 <div className='text-white'>
                     Email
                 </div>
-                <div className='border-white'>
-                    <textarea name="" id="" cols="30" rows="10" className='outline-none p-1 rounded w-full'></textarea>
+                <div className='border border-white'>
+                    <input type="text" name="" id="" className='outline-none p-1 bg-blue w-full text-white'/>
                 </div>
             </div>
-            <div>
-                <input type="submit" value="Send" className='text-white border rounded p-2 m-3'/>
+            <div className='w-3/4'>
+                <div className='text-white'>
+                    Message
+                </div>
+                <div className='border text-white'>
+                    <textarea name="" id="" cols="30" rows="10" className='outline-none p-1 bg-blue w-full text-white'></textarea>
+                </div>
+            </div>
+            <div className='w-3/4 m-3'>
+                <input type="submit" value="Send message" className='text-white border rounded p-2 w-full hover:bg-white hover:text-blue'/>
             </div>
         </form>
     </footer>
